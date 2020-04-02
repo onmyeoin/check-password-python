9007a30843c867912d75edeb0b2f1efd3af09582
Eoin O'Reilly <41189423+onmyeoin@users.noreply.github.com>
2020-04-02 20:06:44 +0100

    Updated with comments

--- checkmypass.py
+++ checkmypass.py
@@ -2,7 +2,7 @@ import requests
 import hashlib
 import sys
 
-
+# sends start of password hash to api
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
@@ -10,6 +10,7 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check API')
     return res
 
+# check password hash against list of potential pw hashes
 def get_password_leak_counts(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
@@ -17,10 +18,14 @@ def get_password_leak_counts(hashes, hash_to_check):
             return count
     return 0
 
+
+# create hash of pw, split into head and tail to send only first 5 chars of hash to api
+# list of hashes that match first 5 chars returned
+# pw hash checked against api response
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    first5_chars, tail = sha1password[:5], sha1password[5:]
-    response = request_api_data(first5_chars)
+    head, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(head)
     return get_password_leak_counts(response, tail)
 
 def main(args):
